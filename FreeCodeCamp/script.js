2dd19b4c06070a8632a625baadd6a410d4b1d144
kotmen <kotmenkot@gmail.com>
2021-11-12 10:30:59 +0200

    2 and 3 task FreeCodeCamp was added

---
+++ FreeCodeCamp/script.js
@@ -0,0 +1,25 @@
+// 1 task
+
+/* This is a
+multi-line comment */
+// This is an in-line comment.
+
+
+// 2 task
+
+var myName;
+
+
+// 3 task
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+
+
+
+
+
+
