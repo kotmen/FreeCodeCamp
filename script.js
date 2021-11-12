ca0687a8039a2d5b248872673c65f9f96795489c
kotmen <kotmenkot@gmail.com>
2021-11-12 17:14:32 +0200

    15 task

---
+++ script.js
@@ -0,0 +1,84 @@
+// 1 task
+
+/* This is a
+multi-line comment */
+// This is an in-line comment.
+
+
+// 2 task
+
+var myName;
+
+
+// 3 task
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+
+// 4 task
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+
+// 5 task
+
+var a = 9;
+
+
+// 6 task
+
+var myFirstName = "Kostya";
+var myLastName = "Kotyk"
+
+// 7 task
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a"
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 8 task
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// 9 task
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// 11 task
+
+const sum = 20 + 0;
+
+// 12 task
+
+const difference = 45 - 33;
+
+// 13 task
+
+const product = 8 * 10;
+
+// 14 task
+
+const quotient = 66 / 33;
+
