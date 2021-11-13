d2a816198b59a89b223ffd10a155e2d109030729
kotmen <kotmenkot@gmail.com>
2021-11-13 17:20:10 +0200

    added 53 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -381,3 +381,35 @@ function functionWithArgs(a, b) {
   }
   
   functionWithArgs(1, 2)
+
+  // 52 task
+
+  function timesFive(num) {
+    return num * 5;
+  }
+  
+  const answer = timesFive(4);
+
+  // 53 task
+
+  // Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+
+function fun1() {
+  
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
