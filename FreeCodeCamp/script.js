dd9bb31692c85b16b75420c5f7f39fa3c3adefe9
kotmen <kotmenkot@gmail.com>
2021-11-14 00:21:47 +0200

    added 55 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -427,3 +427,21 @@ function myOutfit() {
 }
 
 myOutfit();
+
+
+// 55 task
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+
+// Only change code above this line
+
+addThree();
+addFive();
