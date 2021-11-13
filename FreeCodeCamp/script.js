93d2d08cf88a8ddb0d61a480db073ea68bccf1fe
kotmen <kotmenkot@gmail.com>
2021-11-13 11:04:00 +0200

    added 35 tasks

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -226,3 +226,13 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 lastNameLength = lastName.length;
+
+
+// 35 task
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
