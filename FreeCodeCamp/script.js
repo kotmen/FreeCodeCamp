cb6aff9674f235fb329d6ad00f764d592a1f888c
kotmen <kotmenkot@gmail.com>
2021-11-14 00:21:01 +0200

    added 54 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -413,3 +413,17 @@ function fun2() {
   }
   console.log(output);
 }
+
+// 54 task
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
