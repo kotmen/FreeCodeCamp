3076bba2ae0a93b6e468627d447804c39e9c86e8
kotmen <kotmenkot@gmail.com>
2021-11-13 11:57:44 +0200

    added 46 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -325,3 +325,11 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push(["dog", 3]);
+
+// 46 task
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop(); 
