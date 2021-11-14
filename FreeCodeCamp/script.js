aa412b957dd6f28abc76042cf147a73148bf6a80
kotmen <kotmenkot@gmail.com>
2021-11-14 19:07:51 +0200

    added 70 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -655,3 +655,19 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+
+// 70 task
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
