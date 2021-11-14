d38b4704576bbc36fd85eb8defd3996aba1c87f4
kotmen <kotmenkot@gmail.com>
2021-11-14 19:02:55 +0200

    added 69 task

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -637,3 +637,21 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+
+
+// 69 task
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    
+      return "Yes";
+    }
+  
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
