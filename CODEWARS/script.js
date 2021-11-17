33555738f897dce170dc8c853595dfc653bfc944
kotmen <kotmenkot@gmail.com>
2021-11-17 17:43:58 +0200

    2nd task

--- CODEWARS/script.js
+++ CODEWARS/script.js
@@ -3,3 +3,14 @@ function numberToString(num) {
   }
 
 /////////////////////////////////////////////////
+
+String.prototype.toJadenCase = function () {
+  var returnString = [];
+  var words = this.toLowerCase().split(' ');
+  
+  for (var i = 0; i < words.length; i++) {
+      word = words[i];
+      returnString.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return returnString.join(" ");
+};
