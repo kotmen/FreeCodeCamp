da4340715ffafc195d3eafeeb7c58de69c6fcc6f
kotmen <kotmenkot@gmail.com>
2021-11-19 23:36:23 +0200

    77 task complated

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -672,3 +672,151 @@ function testLogicalOr(val) {
 
 testLogicalOr(15);
 
+// 71 task
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else{
+    
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+///// 72 task
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+}
+}
+testElseIf(7);
+//////////// 73 task
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+///////////////// 74 task
+
+function testSize(num) {
+  // Only change code below this line
+  if(num < 5){
+    return 'Tiny';
+  } else if(num < 10) {
+    return 'Small';
+  }  else if(num < 15) {
+    return 'Medium';
+  }  else if(num < 20) {
+    return 'Large';
+  }  else if(num >= 20) {
+    return 'Huge';
+  }
+  // Only change code above this line
+}
+
+testSize(7);
+
+
+///////////////// 75 task
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+  case 1 :
+  answer = "alpha";
+  break;
+  case 2 :
+  answer = "beta";
+  break;
+  case 3 :
+  answer = "gamma";
+  break;
+  default :
+  answer = "delta";
+  break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(3);
+
+
+/////////// 76 task
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 'a':
+  answer = 'apple';
+  break;
+  case 'b':
+  answer = 'bird';
+  break;
+  case 'c':
+  answer = 'cat';
+  break;
+  default:
+  answer = "stuff"
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+/////////////////// 77 task
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(5);
