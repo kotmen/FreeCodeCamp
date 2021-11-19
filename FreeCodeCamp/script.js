4898425f2b6d9d985e7f53ec502a214f717a0ccc
kotmen <kotmenkot@gmail.com>
2021-11-20 01:03:31 +0200

    88 task complated

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -988,3 +988,17 @@ const myDog = {
 };
 
 myDog.bark = "woof";
+
+//////////////// 88 task
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
+
