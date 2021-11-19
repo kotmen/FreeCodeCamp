009e800ff52572cf4c1a3739fb6b003c95e86fc7
kotmen <kotmenkot@gmail.com>
2021-11-20 00:34:16 +0200

    81 task complated

--- FreeCodeCamp/script.js
+++ FreeCodeCamp/script.js
@@ -878,3 +878,35 @@ function abTest(a, b) {
 }
 
 abTest(2,2);
+
+////////////////// 81 task
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 7:
+  case 8:
+  case 9:
+    break;
+  default:
+    count--;
+}
+
+
+  return (count > 0) ? count + ' Bet' : count + ' Hold'; 
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
